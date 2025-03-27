6439cef4f49bffdc74f57aadd85ae9bf5d985ffb
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2025-03-27 04:56:12 +0100

    update app-setup and AppTemplate versions

--- app-setup.js
+++ app-setup.js
@@ -1,25 +1,17 @@
 import { StillAppMixin } from "./@still/component/super/AppMixin.js";
-import { Router } from "./@still/routing/router.js";
 import { Components } from "./@still/setup/components.js";
 import { AppTemplate } from "./app-template.js";
 import { HomeComponent } from "./app/home/HomeComponent.js";
 
 export class StillAppSetup extends StillAppMixin(Components) {
 
-
     constructor() {
         super();
         this.setHomeComponent(HomeComponent);
-        this.setAuthN(true)
     }
 
     async init() {
-
-        const pathAddress = await Router.getComponentFromPath();
-        if (pathAddress)
-            return new AppTemplate(pathAddress);
-        else
-            return new AppTemplate();
+        return await AppTemplate.newApp();
     }
 
 }
