b0aac55101dfe6a1f9f54c3f19e6ef69c2fe8296
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2025-03-28 17:05:34 +0100

    handling exception when handling STILL_HOME variable

--- @still/util/route.js
+++ @still/util/route.js
@@ -1,14 +1,18 @@
 let filePathAltered = false;
 export async function getRoutesFile(defaultFile) {
-    if (STILL_HOME) {
-        const routesFilePath = `${location.origin}/${STILL_HOME}route.map.js`;
-        const _module = await import(routesFilePath);
-        if (!filePathAltered) {
-            filePathAltered = true;
-            console.info(`Components will be routed from ${routesFilePath}`);
+    try {
+
+        if (STILL_HOME) {
+            const routesFilePath = `${location.origin}/${STILL_HOME}route.map.js`;
+            const _module = await import(routesFilePath);
+            if (!filePathAltered) {
+                filePathAltered = true;
+                console.info(`Components will be routed from ${routesFilePath}`);
+            }
+            return _module['stillRoutesMap'];
         }
-        return _module['stillRoutesMap'];
-    }
+
+    } catch (error) { }
 
     return defaultFile;
 }
